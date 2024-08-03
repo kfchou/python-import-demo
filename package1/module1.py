e4e3b48d6dc0684b8ed6236bc60d39826741c8bb
Kenny Chou <kennethfchou@gmail.com>
2024-08-03 21:45:14 +0000

    initial commit

---
+++ package1/module1.py
@@ -0,0 +1,5 @@
+def func1():
+    print("pkg1, module1, func1")
+
+def func1():
+    print("pkg1, module1, func2")
---
+++ package1/module2.py
@@ -0,0 +1,5 @@
+def func1():
+    print("pkg1, module2, func1")
+
+def func1():
+    print("pkg1, module2, func2")
